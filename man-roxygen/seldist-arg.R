a9c1003e0eb0bd4e2e176c6dac07c26283122cb4
aravind-j <aravindjayaramanagri@gmail.com>
2026-05-10 13:31:57 +0530

    Fix typo

--- man-roxygen/seldist-arg.R
+++ man-roxygen/seldist-arg.R
@@ -2,5 +2,5 @@
 #'   distances for selection of individuals from entire collection.
 #' @param hclust.method The hierarchical clustering method to be used. Either
 #'   \code{"ward.D"}, \code{"ward.D2"}, \code{"single"}, \code{"complete"},
-#'   "\code{average"} (= UPGMA), \code{"mcquitty"} (= WPGMA), \code{"median"} (=
+#'   \code{"average"} (= UPGMA), \code{"mcquitty"} (= WPGMA), \code{"median"} (=
 #'   WPGMC) or \code{"centroid"} (= UPGMC).
