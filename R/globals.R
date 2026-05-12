3552b1efc2cf0776e0015184bc56a19970dab994
aravind-j <aravindjayaramanagri@gmail.com>
2026-05-12 17:50:58 +0530

    Update globals

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,8 @@
 if (getRversion() >= "2.15.1")  {
   utils::globalVariables(
     c(
-      "nclust", "idx", "sub_d", "dist.mat", "cassava_EC_gp"
+      "nclust", "idx", "sub_d", "dist.mat", "cassava_EC_gp",
+      "X", "Y", "label"
     )
   )
 }
